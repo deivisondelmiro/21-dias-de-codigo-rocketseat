2253ccbd800cc2a5b207d5ac9282e4742b92d015
Deivison Delmiro <deivisondelmirocor100@gmail.com>
2022-10-14 19:41:13 -0300

    desenvolvimento do dia 08

---
+++ dia-08/js/scrpits.js
@@ -0,0 +1,9 @@
+const body = document.getElementById('body')
+
+const buttonCircule = document.getElementById('buttoCircule')
+
+function mudarCor() {
+    body.style.background = 'gray'
+}
+
+buttonCircle.addEventListener('click', mudarCor)
